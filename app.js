23526825a39fe9ad1a0dd90cdf6b8f358478fb1f
Dadi Sai Krishna <dadisaikrishna39+github@gmail.com>
2024-02-29 17:27:54 +0530

    'first-push'

---
+++ app.js
@@ -0,0 +1,50 @@
+
+      function openlogin(){
+      let login=document.getElementById('login-form')
+      let hero=document.getElementById('hero')
+      hero.style.display='none'
+      login.style.display='block'
+      }
+
+      function closelogin(){
+        let close=document.getElementById('login-form')
+      let card=document.getElementById('hero')
+      card.style.display='block'
+      close.style.display='none'
+      }
+
+      function contact(){
+        let card=document.getElementById('hero')
+      let contactus=document.getElementById('contactus')
+      card.style.display='none'
+      contactus.style.display='block'
+      }
+
+      function closecontact(){
+        let card=document.getElementById('hero')
+      let contactus=document.getElementById('contactus')
+      card.style.display='block'
+      contactus.style.display='none'
+      }
+
+
+      const btn = document.getElementById('button');
+
+      document.getElementById('form')
+       .addEventListener('submit', function(event) {
+         event.preventDefault();
+      
+         btn.value = 'Sending...';
+      
+         const serviceID = 'service_eadh9rd';
+         const templateID = 'template_4c4536t';
+      
+         emailjs.sendForm(serviceID, templateID, this)
+          .then(() => {
+            btn.value = 'Send Email';
+            alert('Form submitted successfully! We will contact you soon.');
+          }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+          });
+      });
